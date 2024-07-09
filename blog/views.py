663b24b9a710532f9ec88fcae7496ebe3e6da5f3
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-09 11:42:06 +0000

    Generic event view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Event
 
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Welcome CrossFit!!")
+class EventList(generic.ListView):
+    queryset = Event.objects.all()
+    template_name = "event_list.html"
