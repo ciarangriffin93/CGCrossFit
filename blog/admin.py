ec5de43faf5a697ac0df2b645333f9ff507b6e95
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-09 14:20:15 +0000

    Add more event

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class EventAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
