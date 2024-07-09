869b1a7ea810a898f1fa517667cb27e9b569e630
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-09 10:09:21 +0000

    Add Secret_key on env.py

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--mp91*%uc*&x0vi=@2m=42h%r0*48dqb(r*&=952(b4bmiy6$k'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
