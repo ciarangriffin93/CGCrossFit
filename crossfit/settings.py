a764690e3b334bec2f35786df299762e3723cbe5
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-10 09:22:19 +0000

    Add script.js on staticfile

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-tgfxwyrylmz.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-xew7cexiyn0.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 
 # Application definition
