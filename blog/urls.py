9dbd1860076b1c2d01db64555dbf899d8f6f4a8c
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-10 10:29:13 +0000

    Add event_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
+    path('<slug:slug>/', views.event_detail, name='event_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Event
 
@@ -7,3 +7,7 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 3
+
+def event_detail(request, slug):
+    event = get_object_or_404(Event, slug=slug)
+    return render(request, 'blog/event_detail.html', {'event': event})
