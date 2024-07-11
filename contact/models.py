a649011e1ac6b389691027cc7af39a081d3cb5d1
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-11 20:06:51 +0000

    Change model contact renamed to contactform

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Contact(models.Model):
+class ContactForm(models.Model):
     
     name = models.CharField(max_length=50, blank=False)
     lastname = models.CharField(max_length=50,
