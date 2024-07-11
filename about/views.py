84d223401868095bb4d6178301f204528c626829
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-11 15:07:39 +0000

    Add view comments and comment on about

--- about/views.py
+++ about/views.py
@@ -5,9 +5,14 @@ from .models import About
 def about_me(request):
    
     about = About.objects.all().order_by('-updated_on').first()
+    comments = about.comments.all().order_by("-created_on")
+    comment_count = about.comments.filter(approved=True).count()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+        "comments": comments,
+        "comment_count": comment_count,
+    },
     )
