49f96309a1bac720333c578bfa5a1abc9e46ba13
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-11 14:36:33 +0000

    Create comment model and admin

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class About(models.Model):
@@ -8,3 +9,18 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    about = models.ForeignKey(
+        About, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
