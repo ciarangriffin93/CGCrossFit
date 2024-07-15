59954052b480764d272a7865653c3247841f72fc
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-15 18:25:23 +0000

    Create post repuest from comment form

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CommentForm
 
@@ -8,6 +9,17 @@ def about_me(request):
     about = About.objects.all().order_by('-updated_on').first()
     comments = about.comments.all().order_by("-created_on")
     comment_count = about.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.about = about
+            comment.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
     comment_form = CommentForm()
 
     return render(
