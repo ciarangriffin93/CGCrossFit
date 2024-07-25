1a38fde4ada9c1bbe92d47ae35b1655f92698be0
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-25 13:57:42 +0000

    Create a style on classes.html

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-wljl426ua5e.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-lahxc2ngqxh.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 
 # Application definition
