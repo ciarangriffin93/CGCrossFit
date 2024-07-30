9b0384532550971284e117d7253fe4e4bc10d51d
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-30 10:32:08 +0000

    Add comment count and comment below the post

--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,14 @@ class EventList(generic.ListView):
 
 def event_detail(request, slug):
     event = get_object_or_404(Event, slug=slug)
+    comments = event.blog_comments.all().order_by("-created_on")
+    comment_count = event.blog_comments.filter(approved=True).count()
+    
     return render(
         request, 
         'blog/event_detail.html', 
         {'event': event,
-        "coder": "Ciaran Griffin"}
+        "comments": comments,
+        "comment_count": comment_count,
+        },
     )
