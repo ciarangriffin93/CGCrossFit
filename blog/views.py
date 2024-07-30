d65894d41c2c32f1c88ae32028cd990e6dc2453e
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-30 10:45:18 +0000

    Add a comment form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Event
+from .forms import CommentForm
 
 # Create your views here.
 class EventList(generic.ListView):
@@ -12,6 +14,19 @@ def event_detail(request, slug):
     event = get_object_or_404(Event, slug=slug)
     comments = event.blog_comments.all().order_by("-created_on")
     comment_count = event.blog_comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.event = event
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
     
     return render(
         request, 
@@ -19,5 +34,6 @@ def event_detail(request, slug):
         {'event': event,
         "comments": comments,
         "comment_count": comment_count,
+        "comment_form": comment_form,
         },
     )
