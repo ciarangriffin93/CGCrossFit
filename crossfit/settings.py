b1277b947a5b996d13ae07404e763ccc55b2ce1f
Ciaran Griffin <griffinciaran3@gmail.com>
2024-08-01 09:41:11 +0000

    Add event excerpt on index.html

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-5ae062wwxm6.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-xenhdn9zd7j.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 
 # Application definition
