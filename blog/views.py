7544f6d9e1ba89ab77c35374bef138cd14919772
Ciaran Griffin <griffinciaran3@gmail.com>
2024-08-02 22:05:41 +0000

    Create likes on view and url

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic
+from django.views import generic, View
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Event, Comment
@@ -15,6 +15,9 @@ class EventList(generic.ListView):
 def event_detail(request, slug):
     event = get_object_or_404(Event, slug=slug)
     comments = event.blog_comments.all().order_by("-created_on")
+    liked = False 
+    if event.likes.filter(id=request.user.id).exists():
+        liked = True
     comment_count = event.blog_comments.filter(approved=True).count()
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
@@ -34,6 +37,7 @@ def event_detail(request, slug):
         request, 
         'blog/event_detail.html', 
         {'event': event,
+        "liked": liked,
         "comments": comments,
         "comment_count": comment_count,
         "comment_form": comment_form,
@@ -60,7 +64,7 @@ def comment_edit(request, slug, comment_id):
 
 def comment_delete(request, slug, comment_id):
     """
-    view  delete comment
+    View delete comment
     """
     queryset = Event.objects.filter(status=1)
     event  = get_object_or_404(queryset, slug=slug)
@@ -70,6 +74,18 @@ def comment_delete(request, slug, comment_id):
         comment.delete()
         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+        messages.add_message(request, messages.ERROR, 'You can only delete your comments!')
 
     return HttpResponseRedirect(reverse('event_detail', args=[slug]))
+
+class EventLike(View):
+
+    def Event(self, request, slug):
+        """Handle POST request to  post likes"""
+        event = get_object_or_404(queryset, slug=slug)
+        if event.likes.filter(id=request.user.id).exists():
+            event.likes.remove(request.user)
+        else:
+            event.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('event_detail', args=[slug]))
