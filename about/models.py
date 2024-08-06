d598492bc25aaaea5f3d287c118000c497eb4771
Ciaran Griffin <griffinciaran3@gmail.com>
2024-08-06 12:47:31 +0000

    Removed comment model on about

--- about/models.py
+++ about/models.py
@@ -10,17 +10,3 @@ class About(models.Model):
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
-    about = models.ForeignKey(
-        About, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
-    approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ["created_on"]
-
-    def __str__(self):
-        return f"Comment {self.body} by {self.author}"
--- about/views.py
+++ about/views.py
@@ -1,34 +1,16 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import About
-from .forms import CommentForm
 
 # Create your views here.
 def about_me(request):
    
     about = About.objects.all().order_by('-updated_on').first()
-    comments = about.comments.all().order_by("-created_on")
-    comment_count = about.comments.filter(approved=True).count()
-    if request.method == "POST":
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.author = request.user
-            comment.about = about
-            comment.save()
-            messages.add_message(
-        request, messages.SUCCESS,
-        'Comment submitted and awaiting approval'
-    )
-    comment_form = CommentForm()
 
     return render(
         request,
         "about/about.html",
         {"about": about,
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form,
     },
     )
 
