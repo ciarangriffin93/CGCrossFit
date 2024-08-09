41b64b740d2dd750930d75e498d1ba5d851e0675
Ciaran Griffin <griffinciaran3@gmail.com>
2024-08-09 18:08:24 +0000

    Add timezome and max participants on booking page

--- booking/views.py
+++ booking/views.py
@@ -1,17 +1,26 @@
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import CrossfitClass, Booking
 
 def class_list(request):
-    classes = CrossfitClass.objects.all()
+    today = timezone.now().date()
+    classes = CrossfitClass.objects.filter(end_time__date__gte=today)
     return render(request, 'booking/class_list.html', {'classes': classes})
 
 def class_detail(request, pk):
     crossfit_class = get_object_or_404(CrossfitClass, pk=pk)
     is_booked = False
+    current_participants = Booking.objects.filter(crossfit_class=crossfit_class).count()
+
     if request.user.is_authenticated:
         is_booked = Booking.objects.filter(user=request.user, crossfit_class=crossfit_class).exists()
-    return render(request, 'booking/class_detail.html', {'crossfit_class': crossfit_class, 'is_booked': is_booked})
+
+    return render(request, 'booking/class_detail.html', {
+        'crossfit_class': crossfit_class,
+        'is_booked': is_booked,
+        'current_participants': current_participants
+    })
 
 @login_required
 def book_class(request, pk):
