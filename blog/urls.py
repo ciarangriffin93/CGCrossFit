9ed76b61800734d9395f9abdd05936f153899edc
Ciaran Griffin <griffinciaran3@gmail.com>
2024-08-15 20:25:08 +0000

    Remove edit comment on event_detail

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
     path('<slug:slug>/', views.event_detail, name='event_detail'),
     path('like/<slug:slug>/', views.event_like, name='event_like'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
-         views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -44,24 +44,6 @@ def event_detail(request, slug):
         },
     )
 
-def comment_edit(request, slug, comment_id):
-    if request.method == "POST":
-        queryset = Event.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comment = get_object_or_404(Comment, pk=comment_id)
-        comment_form = CommentForm(data=request.POST, instance=comment)
-
-        if comment_form.is_valid() and comment.author == request.user:
-            comment = comment_form.save(commit=False)
-            comment.event = post
-            comment.approved = False
-            comment.save()
-            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
-        else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
-
-    return HttpResponseRedirect(reverse('event_detail', args=[slug]))
-
 def comment_delete(request, slug, comment_id):
     """
     View delete comment
