dae1ca2527dae96bd2c14f4dad3e9154fb8ca3d4
Ciaran Griffin <griffinciaran3@gmail.com>
2024-08-21 10:59:17 +0000

    Add Cloudinary static file storage

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-gmu6qzjm70b.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriffi-cgcrossfit-z3goa9tit3u.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 
 # Application definition
@@ -152,6 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
